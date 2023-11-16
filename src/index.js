cc5553a7cdb5e2645ab3b148a639ffc53b9631c6
akankshit.am <akankshit.am@flipkart.com>
2023-11-16 09:31:45 +0530

    Adding icons

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import { join } from "./utils";
 import "./style.css";
+import Icon from "./resources/red-white-cat.jpg";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
